5d750eab0d0091eab6debc1eda1adbf40f11cceb
Kevin Lin <kevinlin@Kevins-Mac.local>
2017-10-10 21:20:37 +0800

    add picker for function

--- Project/MessagePublishController.swift
+++ Project/MessagePublishController.swift
@@ -8,15 +8,19 @@
 
 import UIKit
 
-class MessagePublishController: UIViewController {
+class MessagePublishController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate{
 
     //var device:Device!
     var message:Message!
     var host = "192.168.15.122"
     var port = "51700"
+    var IOfunction = ["向左移入","向內捲入","向外捲入","覆蓋向左","覆蓋向右","覆蓋向上","覆蓋向下","覆蓋向內","附蓋向外","覆蓋 ↑↓","覆蓋 ↓↑","向上捲入","向下捲入","立即顯現","同時出現","跳入","射入","動畫","續幕"]
     
+    @IBOutlet weak var functionPicker: UIPickerView!
     
     @IBOutlet var textField: UITextField!
+    @IBOutlet weak var funcInTextField: UITextField!
+    @IBOutlet weak var funcOutTextField: UITextField!
     
     
     override func viewDidLoad() {
@@ -26,12 +30,74 @@ class MessagePublishController: UIViewController {
         
         title = "Edit line: " + message.line
         // Do any additional setup after loading the view.
+        
+        
+        functionPicker.backgroundColor = UIColor.white
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        return 1
+        
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        
+        return IOfunction.count
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        self.view.endEditing(true)
+        return IOfunction[row]
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        self.funcInTextField.text = self.IOfunction[row]
+        self.functionPicker.isHidden = true
+        
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        if textField == self.funcInTextField {
+            self.functionPicker.isHidden = false
+            //if you dont want the users to se the keyboard type:
+            
+            textField.endEditing(true)
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     @IBAction func pulishMessage(_ sender: AnyObject) {
         let urlString: String = "http://\(host):\(port)/api/Default"
