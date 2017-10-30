ac6b93629a98ee3199c9225ab6eba61b3f7fce72
Kevin Lin <kevinlin@Kevins-Mac.local>
2017-10-30 15:09:31 +0800

    able to publish enable and disable command

--- Project/PickUpLinesTableViewController.swift
+++ Project/PickUpLinesTableViewController.swift
@@ -13,13 +13,38 @@ class PickUpLinesTableViewController: UITableViewController {
     var type: String!
     var device: String!
     
+    // for publish host and port information
+    var host = "192.168.15.110"
+    var port = "51320"
+    
     var messages:[Message] = [
         Message(device: "A1", line: "1", displayTime: "2", funcIn: "A", funcOut: "A", text: "WOW", color: "01"),
         Message(device: "A1", line: "2", displayTime: "2", funcIn: "A", funcOut: "A", text: "YOO", color: "01"),
         Message(device: "A1", line: "3", displayTime: "2", funcIn: "A", funcOut: "A", text: "Such", color: "01"),
         Message(device: "A1", line: "4", displayTime: "2", funcIn: "A", funcOut: "A", text: "Display", color: "01"),
-        Message(device: "A1", line: "10", displayTime: "2", funcIn: "A", funcOut: "A", text: "Doge", color: "01")
+        Message(device: "A1", line: "5", displayTime: "2", funcIn: "A", funcOut: "A", text: "WOW", color: "01"),
+        Message(device: "A1", line: "6", displayTime: "2", funcIn: "A", funcOut: "A", text: "YOO", color: "01"),
+        Message(device: "A1", line: "7", displayTime: "2", funcIn: "A", funcOut: "A", text: "Such", color: "01"),
+        Message(device: "A1", line: "8", displayTime: "2", funcIn: "A", funcOut: "A", text: "Display", color: "01"),
+        Message(device: "A1", line: "9", displayTime: "2", funcIn: "A", funcOut: "A", text: "WOW", color: "01"),
+        Message(device: "A1", line: "10", displayTime: "2", funcIn: "A", funcOut: "A", text: "YOO", color: "01"),
+        Message(device: "A1", line: "11", displayTime: "2", funcIn: "A", funcOut: "A", text: "Such", color: "01"),
+        Message(device: "A1", line: "12", displayTime: "2", funcIn: "A", funcOut: "A", text: "Display", color: "01"),
+        Message(device: "A1", line: "1", displayTime: "2", funcIn: "A", funcOut: "A", text: "WOW", color: "01"),
+        Message(device: "A1", line: "2", displayTime: "2", funcIn: "A", funcOut: "A", text: "YOO", color: "01"),
+        Message(device: "A1", line: "3", displayTime: "2", funcIn: "A", funcOut: "A", text: "Such", color: "01"),
+        Message(device: "A1", line: "4", displayTime: "2", funcIn: "A", funcOut: "A", text: "Display", color: "01"),
+        Message(device: "A1", line: "5", displayTime: "2", funcIn: "A", funcOut: "A", text: "WOW", color: "01"),
+        Message(device: "A1", line: "6", displayTime: "2", funcIn: "A", funcOut: "A", text: "YOO", color: "01"),
+        Message(device: "A1", line: "7", displayTime: "2", funcIn: "A", funcOut: "A", text: "Such", color: "01"),
+        Message(device: "A1", line: "8", displayTime: "2", funcIn: "A", funcOut: "A", text: "Display", color: "01"),
+        Message(device: "A1", line: "9", displayTime: "2", funcIn: "A", funcOut: "A", text: "WOW", color: "01"),
+        Message(device: "A1", line: "10", displayTime: "2", funcIn: "A", funcOut: "A", text: "YOO", color: "01"),
+        Message(device: "A1", line: "11", displayTime: "2", funcIn: "A", funcOut: "A", text: "Such", color: "01"),
+        Message(device: "A1", line: "12", displayTime: "2", funcIn: "A", funcOut: "A", text: "Display", color: "01")
     ]
+    
+    var setOfLines = Set<Int>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,15 +55,21 @@ class PickUpLinesTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.trash,  target: self, action: nil)
+        
         
         tableView.backgroundColor = UIColor.darkGray
         
+        
+        
         if type == "B6" {
             title = "Enable Lines On " + device
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add,  target: self, action: #selector(PickUpLinesTableViewController.editAction))
         } else if type == "B7" {
             title = "Disable Lines On " + device
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.trash,  target: self, action: #selector(PickUpLinesTableViewController.editAction))
         }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -76,10 +107,72 @@ class PickUpLinesTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let cell = tableView.cellForRow(at: indexPath)
+        cell?.tintColor = .white
         cell?.accessoryType = .checkmark
         
-        tableView.deselectRow(at: indexPath, animated: false)
+        setOfLines.insert(indexPath.row)
     }
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.tintColor = .white
+        cell?.accessoryType = .none
+        
+        setOfLines.remove(indexPath.row)
+    }
+    
+    func editAction() {
+        if setOfLines.count != 0 {
+            let urlString: String = "http://\(host):\(port)/api/Mqtt"
+            let url = URL(string: urlString)!
+            
+            let now = Date()
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+            
+            var request = URLRequest(url: url)
+            
+            let count = setOfLines.count
+            
+            let linesToEdit = setOfLines.sorted()
+            var lineNumbers = "\(linesToEdit[0]+1)"
+            
+            for line in linesToEdit {
+                if line == linesToEdit[0] {
+                    continue
+                }
+                
+                lineNumbers += ",\(line+1)"
+            }
+            
+            
+            let body = "Topic=\(device!)&Type=\(type!)&Data=\(count)," + lineNumbers + "&Date=\(dateFormatter.string(from: now))"
+            
+            
+            print (body)
+            
+            let postData = body.data(using: String.Encoding.utf8)
+            
+            request.httpMethod = "POST"
+            request.httpBody = postData
+            
+            request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+            
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if error != nil {
+                    print(error as Any)
+                } else {
+                    guard let data = data else {return}
+                    print(data)
+                }
+                
+            }
+            task.resume()
+        }
+        
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
 
     /*
     // Override to support conditional editing of the table view.
